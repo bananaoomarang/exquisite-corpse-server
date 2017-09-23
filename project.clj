93884e6e32c7f2242100de8eb49dce8730c6f5eb
Milo Mordaunt <milomord@gmail.com>
2017-09-23 10:20:23 -0400

    attempt at sente

--- project.clj
+++ project.clj
@@ -7,9 +7,12 @@
                  [com.novemberain/monger "3.1.0"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]
-                 [jumblerg/ring.middleware.cors "1.0.1"]]
+                 [jumblerg/ring.middleware.cors "1.0.1"]
+                 [http-kit "2.2.0"]
+                 [com.taoensso/sente "1.11.0"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler exquisite-corpse-server.handler/app}
+  :ring {:handler exquisite-corpse-server.core/app}
+  :main exquisite-corpse-server.core
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- src/exquisite_corpse_server/core.clj
+++ src/exquisite_corpse_server/core.clj
@@ -1,26 +1,50 @@
-(ns exquisite-corpse-server.handler
+(ns exquisite-corpse-server.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [org.httpkit.server :refer [run-server]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.cors :refer [wrap-cors]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
             [monger.core :as mg]
-            [monger.collection :as mc])
+            [monger.collection :as mc]
+            [taoensso.sente :as sente]
+            [taoensso.sente.server-adapters.http-kit :refer (get-sch-adapter)])
   (:import org.bson.types.ObjectId))
 
 (def conn (mg/connect))
 (def db   (mg/get-db conn "exquisite_stories"))
 
+(let [{:keys [ch-recv send-fn connected-uids
+              ajax-post-fn ajax-get-or-ws-handshake-fn]}
+      (sente/make-channel-socket! (get-sch-adapter) {})]
+  (def ring-ajax-post                ajax-post-fn)
+  (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
+  (def ch-chsk                       ch-recv)
+  (def chsk-send!                    send-fn)
+  (def connected-uids                connected-uids))
+
 (defn handle-get [id]
   (let [doc (mc/find-one-as-map db "stories" { :_id (ObjectId. id)})]
     { :body { :id (.toString (:_id doc)) :story (:story doc) }}))
 
+(defn test-fast-server>user-pushes
+  "Quickly pushes 100 events to all connected users. Note that this'll be
+  fast+reliable even over Ajax!"
+  []
+  (doseq [uid (:any @connected-uids)]
+    (doseq [i (range 100)]
+      (chsk-send! uid [:some/thing (str "hello " i "!!")]))))
+
 (defn handle-get-random []
+  (chsk-send! (:any @connected-uids) [:some/thing (str "hello!!")])
+  (println "SENT")
   (let [docs (mc/aggregate db "stories" [{:$sample { :size 1 }}])
         doc  (first docs)]
     { :body
      { :id (-> doc :_id .toString)
-       :story (:story doc)}}))
+      :story (:story doc)}}))
 
 (defn handle-post [body]
   (let [doc (mc/insert-and-return db "stories" { :story (:story body) })]
@@ -36,11 +60,18 @@
   (GET "/story/:id" [id] (handle-get id))
   (POST "/story" { body :body } (handle-post body))
   (PATCH "/story/:id" request (handle-patch (-> request :params :id) (-> request :body :nextLine)))
+  (GET "/chsk" req (ring-ajax-get-or-ws-handshake req))
+  (POST "/chsk" req (ring-ajax-post req))
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
+      (wrap-defaults api-defaults)
       (wrap-cors #".*")
       (wrap-json-body { :keywords? true })
       (wrap-json-response)
-      (wrap-defaults api-defaults)))
+      (wrap-keyword-params)
+      (wrap-params)))
+
+(defn -main []
+  (run-server app {:port 3000}))
