e006e29c5b4977bb1c4717a766daa3271e02dd28
Milo Mordaunt <milomord@gmail.com>
2017-10-28 14:18:16 -0400

    update deps

--- project.clj
+++ project.clj
@@ -2,20 +2,22 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
+                 [compojure "1.6.0"]
                  [com.novemberain/monger "3.1.0"]
-                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-defaults "0.3.1"]
                  [ring/ring-json "0.4.0"]
                  [ring/ring-core "1.6.2"]
                  [ring/ring-devel "1.6.2"]
                  [jumblerg/ring.middleware.cors "1.0.1"]
                  [http-kit "2.2.0"]
                  [danlentz/clj-uuid "0.1.7"]
-                 [jarohen/chord "0.8.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [jarohen/chord "0.8.1"]
+                 [org.clojure/core.async "0.3.443"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-ancient "0.6.14"]]
   :ring {:handler exquisite-corpse-server.core/app}
   :main exquisite-corpse-server.core
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.1"]]}})
