e378823d81e1162cb3d43229f87a3a659d0106e3
Milo Mordaunt <milomord@gmail.com>
2017-10-29 13:09:17 -0400

    downgrade to stable clojure

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.6.0"]
                  [com.novemberain/monger "3.1.0"]
                  [ring/ring-defaults "0.3.1"]
